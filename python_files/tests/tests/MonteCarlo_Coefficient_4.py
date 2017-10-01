10f795fca8f28496d28907b759860549cf7e1f64
Tii1 <keil.menden@web.de>
2017-10-01 17:59:56 +0200

    Final update!

--- python_files/tests/tests/MonteCarlo_Coefficient_4.py
+++ python_files/tests/tests/MonteCarlo_Coefficient_4.py
@@ -14,7 +14,7 @@ from gridlod import interp, coef, util, fem, world, linalg, femsolver
 import pg_rand, femsolverCoarse, buildcoef2d
 from gridlod.world import World
 
-def result(pglod, world, CoefClass, A, f, MC=1, numberofcorrectors=60, MCplot=None, ROOT=None):
+def result(pglod, world, CoefClass, A, f, MC=1, prob=100):
     NWorldFine = world.NWorldFine
     NWorldCoarse = world.NWorldCoarse
     NCoarseElement = world.NCoarseElement
@@ -23,97 +23,120 @@ def result(pglod, world, CoefClass, A, f, MC=1, numberofcorrectors=60, MCplot=No
     NpFine = np.prod(NWorldFine+1)
     NpCoarse = np.prod(NWorldCoarse+1)
     
+    plist = [0,5,10,20,30,100]
+    
     #### initial #####
-    xmLoda = []
-    xmVcLoda = []
-    xmLodVcLoda = []
+    xmLoda = np.zeros([MC,np.size(plist)])
+    xmVcLoda = np.zeros([MC,np.size(plist)])
+    xmLodVcLoda = np.zeros([MC,np.size(plist)])
     
     ems = []
     
-    plottingx = []
-    plottingy = []
-    plottingz = []
+    plottingx = np.zeros([MC-1,np.size(plist)])
+    plottingy = np.zeros([MC-1,np.size(plist)])
+    plottingz = np.zeros([MC-1,np.size(plist)])
+        
+    plotting2x = np.zeros([MC-1,np.size(plist)])
+    plotting2y = np.zeros([MC-1,np.size(plist)])
+    plotting2z = np.zeros([MC-1,np.size(plist)])
     
-    plotting2x = []
-    plotting2y = []
-    plotting2z = []
-    
-    plotting3x = []
-    plotting3y = []
-    plotting3z = []
-    
+    plotting3x = np.zeros([MC-1,np.size(plist)])
+    plotting3y = np.zeros([MC-1,np.size(plist)])
+    plotting3z = np.zeros([MC-1,np.size(plist)])
+
     for i in range(0,MC):
         print '_____Sample__ ' + str(i+1) + '/' + str(MC) + ' ____' 
-        xmLod, xmVcLod, xmLodVcLod = VcLod(pglod, world, CoefClass, f, prob=20, numberofcorrectors=numberofcorrectors, m=i)
-        xmLoda.append(xmLod)
-        xmVcLoda.append(xmVcLod)
-        xmLodVcLoda.append(xmLodVcLod)
-    
-        muLod = 0
-        muVcLod = 0
-        muLodVcLod = 0
+        R = CoefClass.RandomVanish( probfactor      = prob,
+                                    PartlyVanish    = None,
+                                    Original        = True)
+        
+        ANew = R.flatten()
+        
+        ###### Reference solution ######
+        f_fine = np.ones(NpFine)
+        uFineFem, AFine, MFine = femsolver.solveFine(world, ANew, f_fine, None, boundaryConditions)
+        
+        Anew = coef.coefficientFine(NWorldCoarse, NCoarseElement, ANew)
+        
+        ###### tolerance = 0 without computing ######
+        vis, eps = pglod.updateCorrectors(Anew, 0, f, 1, clearFineQuantities=False, mc=True, Computing=None)
+        print 'Affected correctors: ' + str(np.sum(vis))
+        
+        ##### VCLOD ######
+        uVc = []
+        updated = 0
+        for p in plist:
+            print 'p = ' + str(p) + '%',
+            uVcLod, updated = VcLod(pglod, world, Anew, eps, updated, numberofcorrectors=p)
+            if p == 100:
+                uLod = uVcLod
+                pglod.CorrectorsToOrigin()
+            else:
+                uVc.append(uVcLod)
+        
+        for k in range(0,np.shape(uVc)[0]):
+            uVcLod = uVc[k]
+            eVcLod = np.sqrt(np.dot(uFineFem - uVcLod, MFine*(uFineFem - uVcLod))) / np.sqrt(np.dot(uFineFem, MFine*uFineFem))
+            eLodVcLod = np.sqrt(np.dot(uVcLod - uLod, MFine*(uVcLod - uLod))) / np.sqrt(np.dot(uLod, MFine*uLod))
+            eLod = np.sqrt(np.dot(uFineFem - uLod, MFine*(uFineFem - uLod))) / np.sqrt(np.dot(uFineFem, MFine*uFineFem))
+            
+            xmLoda[i,k] = eLod
+            xmVcLoda[i,k] = eVcLod
+            xmLodVcLoda[i,k] = eLodVcLod
+           
         if i == 0:
             continue
-            
-        for j in range(0,i+1):
-            muLod += xmLoda[j]
-            muVcLod += xmVcLoda[j]
-            muLodVcLod += xmLodVcLoda[j]
-    
-        muLod /= i+1
-        muVcLod /= i+1
-        muLodVcLod /= i+1
-    
-        sig2Lod = 0
-        sig2VcLod = 0
-        sig2LodVcLod = 0
-    
-        for j in range(0,i+1):
-            sig2Lod += (xmLoda[j]-muLod)**(2)
-            sig2VcLod += (xmVcLoda[j]-muVcLod)**(2)
-            sig2LodVcLod += (xmLodVcLoda[j]-muLodVcLod)**(2)
-        
-        sig2Lod /= i
-        sig2VcLod /= i
-        sig2LodVcLod /= i
-    
-        a = [np.sqrt(sig2Lod)/np.sqrt(i+1)*1.96,np.sqrt(sig2VcLod)/np.sqrt(i+1)*1.96,np.sqrt(sig2LodVcLod)/np.sqrt(i+1)*1.96]
-        mum = [muLod,muVcLod,muLodVcLod]
-        
-        plottingx.append(mum[0]-a[0])
-        plottingy.append(mum[0])
-        plottingz.append(mum[0]+a[0])
-        
-        plotting2x.append(mum[1]-a[1])
-        plotting2y.append(mum[1])
-        plotting2z.append(mum[1]+a[1])
-
-        plotting3x.append(mum[1]-a[1])
-        plotting3y.append(mum[1])
-        plotting3z.append(mum[1]+a[1])
-        
         ems.append(i+1)
-        Matrix = CoefClass.Matrix.flatten()
-        safer(ROOT, mum, a, plottingx, plottingy, plottingz, plotting2x, plotting2y, plotting2z, plotting3x, plotting3y, plotting3z, ems, Matrix)
+        
+        for k in range(0,np.shape(uVc)[0]):
+            muLod = 0
+            muVcLod = 0
+            muLodVcLod = 0
+            for j in range(0,i+1):
+                muLod += xmLoda[j,k]
+                muVcLod += xmVcLoda[j,k]
+                muLodVcLod += xmLodVcLoda[j,k]
+    
+            muLod /= i+1
+            muVcLod /= i+1
+            muLodVcLod /= i+1
+    
+            sig2Lod = 0
+            sig2VcLod = 0
+            sig2LodVcLod = 0
+    
+            for j in range(0,i+1):
+                sig2Lod += (xmLoda[j,k]-muLod)**(2)
+                sig2VcLod += (xmVcLoda[j,k]-muVcLod)**(2)
+                sig2LodVcLod += (xmLodVcLoda[j,k]-muLodVcLod)**(2)
+        
+            sig2Lod /= i
+            sig2VcLod /= i
+            sig2LodVcLod /= i
+    
+            a = [np.sqrt(sig2Lod)/np.sqrt(i+1)*1.96,np.sqrt(sig2VcLod)/np.sqrt(i+1)*1.96,np.sqrt(sig2LodVcLod)/np.sqrt(i+1)*1.96]
+            mum = [muLod,muVcLod,muLodVcLod]
+            
+            plottingx[i-1,k] = mum[0]-a[0]
+            plottingy[i-1,k] = mum[0]
+            plottingz[i-1,k] = mum[0]+a[0]
+        
+            plotting2x[i-1,k] = mum[1]-a[1]
+            plotting2y[i-1,k] = mum[1]
+            plotting2z[i-1,k] = mum[1]+a[1]
+
+            plotting3x[i-1,k] = mum[2]-a[2]
+            plotting3y[i-1,k] = mum[2]
+            plotting3z[i-1,k] = mum[2]+a[2]
+            
+            Matrix = CoefClass.Matrix.flatten()
+            ROOT = '../test_data/MonteCarlo/Coef1/p' + str(100/prob) + '/' + str(plist[k]) 
+            safer(ROOT, mum, a, plottingx[:,k], plottingy[:,k], plottingz[:,k], plotting2x[:,k], plotting2y[:,k], plotting2z[:,k], plotting3x[:,k], plotting3y[:,k], plotting3z[:,k], ems, Matrix)
         
     return a,mum
     
-def Errors(world, R, uLod, uVcLod):
-    NWorldFine = world.NWorldFine
-    NpFine = np.prod(NWorldFine+1)
-    
-    ANew = R.flatten()
-    boundaryConditions = world.boundaryConditions
-    f_fine = np.ones(NpFine)
-    uFineFem, AFine, MFine = femsolver.solveFine(world, ANew, f_fine, None, boundaryConditions)
-    
-    errorLod = np.sqrt(np.dot(uFineFem - uLod, MFine*(uFineFem - uLod))) / np.sqrt(np.dot(uFineFem, MFine*uFineFem))
-    errorVcLod = np.sqrt(np.dot(uFineFem - uVcLod, MFine*(uFineFem - uVcLod))) / np.sqrt(np.dot(uFineFem, MFine*uFineFem))
-    errorLodVcLod = np.sqrt(np.dot(uVcLod - uLod, MFine*(uVcLod - uLod))) / np.sqrt(np.dot(uLod, MFine*uLod))
-    
-    return errorLod, errorVcLod, errorLodVcLod
-    
-def VcLod(pglod, world, CoefClass,f, prob=20, numberofcorrectors=20, m=0):
+def VcLod(pglod, world, Anew, eps, updated = 0,
+    numberofcorrectors=5):
     NWorldFine = world.NWorldFine
     NWorldCoarse = world.NWorldCoarse
     NCoarseElement = world.NCoarseElement
@@ -121,55 +144,34 @@ def VcLod(pglod, world, CoefClass,f, prob=20, numberofcorrectors=20, m=0):
     boundaryConditions = world.boundaryConditions
     NpFine = np.prod(NWorldFine+1)
     NpCoarse = np.prod(NWorldCoarse+1)
-
-    R = CoefClass.RandomVanish( probfactor      = prob,
-                                PartlyVanish    = None,
-                                Original        = True)
-
-    #new Coefficient
-    ANew = R.flatten()
-    Anew = coef.coefficientFine(NWorldCoarse, NCoarseElement, ANew)
-
-    ###### tolerance = 0 ######
-    vis, eps = pglod.updateCorrectors(Anew, 0, f, 1, clearFineQuantities=False, mc=True)
-    print 'Affected correctors: ' + str(np.sum(vis))
-    KFull = pglod.assembleMsStiffnessMatrix()
-    MFull = fem.assemblePatchMatrix(NWorldCoarse, world.MLocCoarse)
-    free  = util.interiorpIndexMap(NWorldCoarse)
-
-    bFull = MFull*f
-    KFree = KFull[free][:,free]
-    bFree = bFull[free]
-
-    xFree = sparse.linalg.spsolve(KFree, bFree)
-    basis = fem.assembleProlongationMatrix(NWorldCoarse, NCoarseElement)
-
-    basisCorrectors = pglod.assembleBasisCorrectors()
-    modifiedBasis = basis - basisCorrectors
-
-    xFull = np.zeros(NpCoarse)
-    xFull[free] = xFree
-    uCoarse = xFull
-    uLod = modifiedBasis*xFull
 
     ##### tolerance = certain ######
     eps = filter(lambda x: x!=0, eps)
     eps.sort()
     epssize = np.size(eps)
-    until = int(round(numberofcorrectors/100. * epssize,0))
-    
+    until = int(round((numberofcorrectors/100. * epssize) +0.49,0))
+    if epssize != 0:
+        until = int(round((until * 256./epssize)+0.49,0))
     tolrev = []
     for i in range(epssize-1,-1,-1):
         tolrev.append(eps[i])
     
-    tol = tolrev[until]
-    
-    vistol = pglod.updateCorrectors(Anew, tol, f, clearFineQuantities=False)
-    print 'Updated correctors: ' + str(np.sum(vistol))
+    if epssize == 0:
+        print 'nothing to update'
+    else:
+        if until >= epssize:
+            tol = 0
+        else:
+            tol = tolrev[until]
+
+        vistol = pglod.updateCorrectors(Anew, tol, f, clearFineQuantities=False, mc=True, Testing=True)
+        updated += np.sum(vistol)
+        print 'Updated correctors: ' + str(updated)
+        
     KFull = pglod.assembleMsStiffnessMatrix()
     MFull = fem.assemblePatchMatrix(NWorldCoarse, world.MLocCoarse)
     free  = util.interiorpIndexMap(NWorldCoarse)
-
+   
     bFull = MFull*f
     KFree = KFull[free][:,free]
     bFree = bFull[free]
@@ -185,9 +187,7 @@ def VcLod(pglod, world, CoefClass,f, prob=20, numberofcorrectors=20, m=0):
     uCoarse = xFull
     uVcLod = modifiedBasis*xFull
 
-    xmLod, xmVcLod, xmLodVcLod = Errors(world, R, uLod, uVcLod)
-
-    return xmLod, xmVcLod, xmLodVcLod
+    return uVcLod, updated
     
 #background
 bg = 0.05
@@ -262,28 +262,16 @@ Aold = coef.coefficientFine(NWorldCoarse, NCoarseElement, ABase)
 pglod = pg_rand.VcPetrovGalerkinLOD(Aold, world, k, IPatchGenerator, 0)
 pglod.originCorrectors(clearFineQuantities=False)
 
-#correctors that we will update in %
-numberofcorrectors = 20
+#Perturbations
+print '_____________ 1% Perturbations __________'
+prob = 100
 
-MC = 50
-ROOT = '../../../test_data/MonteCarlo/Coef4/p20'
-a, mum = result(pglod, world, CoefClass, A, f, MC, numberofcorrectors, MCplot=True, ROOT=ROOT)
+MC = 100
+a, mum = result(pglod, world, CoefClass, A, f, MC, prob)
 
-print 'p = '+ str(20) + '%'
-print 'uLod  ' + str(mum[0]) + ' +- ' + str(a[0])
-print 'uVcLod ' + str(mum[1]) + ' +- ' + str(a[1])
-print 'uLodVcLod ' + str(mum[2]) + ' +- ' + str(a[2])
+#Perturbations
+print '_____________ 2% Perturbations __________'
+prob = 50
 
-#correctors that we will update in %
-numberofcorrectors = 40
-
-ROOT = '../../../test_data/MonteCarlo/Coef4/p40'
-a, mum = result(pglod, world, CoefClass, A, f, MC, numberofcorrectors, MCplot=True, ROOT=ROOT)
-
-print 'p = '+ str(40) + '%'
-print 'uLod  ' + str(mum[0]) + ' +- ' + str(a[0])
-print 'uVcLod ' + str(mum[1]) + ' +- ' + str(a[1])
-print 'uLodVcLod ' + str(mum[2]) + ' +- ' + str(a[2])
-
-plt.show()
-
+MC = 100
+a, mum = result(pglod, world, CoefClass, A, f, MC, prob)
